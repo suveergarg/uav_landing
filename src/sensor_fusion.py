a2a79c295b61fc1aa37ae735937eb1f9f8610b50
gsuveer <gargsuveer@gmail.com>
2020-04-19 19:23:24 -0400

    With Visual Odometry

---
+++ src/sensor_fusion.py
@@ -0,0 +1,26 @@
+import rospy
+from geometry_msgs.msg import TwistWithCovarianceStamped
+from geometry_msgs.msg import PoseWithCovarianceStamped
+
+pub_twist = rospy.Publisher('/twistupdate', TwistWithCovarianceStamped, queue_size=10)
+pub_pose = rospy.Publisher('/poseupdate',   PoseWithCovarianceStamped, queue_size=10)
+
+def callback(msg):
+	twist = TwistWithCovarianceStamped()
+	pose  = PoseWithCovarianceStamped()	
+
+	pose = 
+	
+	pub_pose.publish(pose)
+	pub_twist.publish(twist)
+
+def init():
+  rospy.init_node('sensor_fusion_node')
+  rospy.Subscriber('/mono_odometer/odometry', Image, callback)
+  
+  while not rospy.is_shutdown():
+    rospy.spin()
+
+if __name__ == '__main__':
+  init()
+
